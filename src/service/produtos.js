57b8370cd55c3bf84766aa487474bab4cc3ca52b
YoruH03 <yuri03ysl@gmail.com>
2023-12-09 21:30:37 -0300

    Search consertado

--- src/service/produtos.js
+++ src/service/produtos.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import BASE_URL from "./base_url";
 
 function pegaTodosProdutos(){
-    const promise = axios.get(BASE_URL+'/produtos')
+    const promise = axios.get(BASE_URL+'/produto')
     return promise
 }
 
